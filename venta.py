c9b4a841e7f13b5789922043c6d30cbf2fee4bc4
Daniel <Danielcasaspira>
2020-11-29 21:47:02 -0500

    add prueba23

--- venta.py
+++ venta.py
@@ -3,9 +3,9 @@ import requests
 class Venta():
 
     def realizarVenta(self, producto, cantidad):
-        requests.get('http://localhost:8081/facturas/nueva-factura/Tony_Stark')
+        requests.get('http://127.0.0.1:8081/facturas/nueva-factura/Tony_Stark')
 
-        r = requests.get('http://localhost:8081/facturas/'+producto+'/'+cantidad)
+        r = requests.get('http://127.0.0.1:8081/facturas/'+producto+'/'+cantidad)
         
         item_object = r.json()
 
